e24c8a82d79dee8a2dcb755ce5610da806c8fd8c
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-01-26 00:51:19 -0500

    Fix burger bug

--- src/scripts/includes/menu.js
+++ src/scripts/includes/menu.js
@@ -1,15 +1,21 @@
 window.Menu = {
-	
+
 	burger: null,
 
 	init: async function() {
 		this.burger = document.querySelector('.burger');
 		this.burger.addEventListener('click', () => this.toggleBurger());
-		document.addEventListener('click', () => this.burger.classList.remove('open'), true);
+		document.addEventListener('click', (e) => this.closeBurger(e), true);
 	},
 
 	toggleBurger: async function() {
 		this.burger.classList.toggle('open');
+	},
+
+	closeBurger: async function(e) {
+		if(!e.target.classList.contains('burger')) {
+			this.burger.classList.remove('open')
+		}
 	}
 
 }
