5b0e1252390b3cfcc0af32a0180ca7faf5f3f8f7
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-01-30 00:38:56 -0500

    Menu resize

--- src/scripts/includes/menu.js
+++ src/scripts/includes/menu.js
@@ -1,20 +1,49 @@
 window.Menu = {
 
 	burger: null,
+	maxscroll: -1,
+	scrollpos: -1,
+
 
 	init: async function() {
 		this.burger = document.querySelector('.burger');
 		this.burger.addEventListener('click', () => this.toggleBurger());
 		document.addEventListener('click', (e) => this.closeBurger(e), true);
+		document.addEventListener('scroll', () => this.setScrollY(), { passive: true });
+		window.addEventListener('resize', () => this.setPaddingTop(), { passive: true });
+		this.setPaddingTop();
 	},
+
 
 	toggleBurger: async function() {
 		this.burger.classList.toggle('open');
 	},
 
+
 	closeBurger: async function(e) {
 		if(!e.target.classList.contains('burger')) {
 			this.burger.classList.remove('open')
+		}
+	},
+
+
+	setScrollY: async function() {
+ 		let pos = window.scrollY || document.documentElement.scrollTop;
+		if(pos > this.maxscroll) pos = this.maxscroll;
+		const perc = pos / this.maxscroll;
+		if(perc != this.scrollpos) {
+			this.scrollpos = perc;
+			await new Promise(requestAnimationFrame);
+			document.documentElement.style.setProperty('--scroll-pos', String(this.scrollpos));
+		}
+	},
+
+
+	setPaddingTop: async function() {
+		const padding = parseFloat(getComputedStyle(document.documentElement).scrollPaddingTop);
+		if(padding != this.maxscroll) {
+			this.maxscroll = padding;
+			this.setScrollY();
 		}
 	}
 
