c98c782474d38bec6c960d0be8776492466fef9d
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-01-31 21:54:59 -0500

    Preload metadata

--- dist/scripts/mei.core.min.js
+++ dist/scripts/mei.core.min.js
@@ -7,7 +7,7 @@
      ╚████╔╝ ███████╗╚██████╔╝███████╗██╗
       ╚═══╝  ╚══════╝ ╚══▀▀═╝ ╚══════╝╚═╝
 
-    Généré: Samedi le 31 janvier 2026 à 21 h 52
+    Généré: Samedi le 31 janvier 2026 à 21 h 54
     Auteur: Maxime Larrivée-Roy <mlarriveeroy@gmail.com>
     Github: https://github.com/meiquebec
 
