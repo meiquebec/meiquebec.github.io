5d65c91efe25deb6f4bd04d3f1c77128c7d0d907
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-02-15 16:43:51 -0500

    Sécurité

--- src/scripts/mei.core.js
+++ src/scripts/mei.core.js
@@ -1,5 +1,5 @@
-import './includes/secrets';
 import './includes/helpers';
+import './includes/secrets';
 import './includes/menu';
 import './includes/gallery';
 import './includes/carte';
@@ -7,4 +7,4 @@ import './includes/mapstyle';
 
 const driftTime = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--drift-time').replace(/s$/gi, ''));
 const delay = 0 - (Math.floor(Math.random() * (driftTime + 1)));
-requestAnimationFrame(() => document.documentElement.style.setProperty('--drift-time-delay',`${delay}s`));
+document.documentElement.style.setProperty('--drift-time-delay',`${delay}s`);
